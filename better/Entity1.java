11231c9430170fce3d0903a9209ef8b6a534eb42
Mohit Gupta <mohit.gupta.cse11@iitbhu.ac.in>
2018-11-09 17:35:09 -0500

    implement poison reverse, test using seed value 12

---
+++ better/Entity1.java
@@ -0,0 +1,79 @@
+public class Entity1 extends Entity
+{    
+    // Perform any necessary initialization in the constructor
+    public Entity1()
+    {
+        nodeNumber = 1;
+        neighbours = new int[]{0,2};
+        initializeDistanceTable();
+    }
+    
+    // Handle updates when a packet is received.  Students will need to call
+    // NetworkSimulator.toLayer2() with new packets based upon what they
+    // send to update.  Be careful to construct the source and destination of
+    // the packet correctly.  Read the warning in NetworkSimulator.java for more
+    // details.
+    // public void update(Packet p)
+    // {
+    // }
+    
+     public void linkCostChangeHandler(int whichLink, int newCost)
+    {
+        int oldValue = distanceTable[whichLink][nodeNumber];
+        distanceTable[whichLink][nodeNumber] = newCost;
+        distanceTable[whichLink][whichLink] = newCost;
+        viaDistanceVector[whichLink] = nodeNumber;
+        System.out.println("The link cost between node " + nodeNumber + " and " + whichLink + " changed to new value " + newCost);
+        int minValue = 999;
+        for (int j=0; j < NetworkSimulator.NUMENTITIES; j++) {
+            if (distanceTable[whichLink][j] < minValue) {
+                minValue = distanceTable[whichLink][j];
+                viaDistanceVector[whichLink] = j;
+            }
+        }
+        if (minValue != oldValue) {
+            distanceTable[whichLink][nodeNumber] = minValue;
+            System.out.println("Node " + nodeNumber + " says: the link cost change affected my distance vector");
+            printDT();
+            sendToNeighbours();
+        } else {
+            System.out.println("Node " + nodeNumber + " says: the link cost change did not affect my distance vector");
+        }
+    }
+    
+    public void printDT()
+    {
+        System.out.println();
+        System.out.println("         via");
+        System.out.println(" D1 |   0   2");
+        System.out.println("----+--------");
+        for (int i = 0; i < NetworkSimulator.NUMENTITIES; i++)
+        {
+            if (i == 1)
+            {
+                continue;
+            }
+            
+            System.out.print("   " + i + "|");
+            for (int j = 0; j < NetworkSimulator.NUMENTITIES; j +=2)
+            {
+            
+                if (distanceTable[i][j] < 10)
+                {    
+                    System.out.print("   ");
+                }
+                else if (distanceTable[i][j] < 100)
+                {
+                    System.out.print("  ");
+                }
+                else 
+                {
+                    System.out.print(" ");
+                }
+                
+                System.out.print(distanceTable[i][j]);
+            }
+            System.out.println();
+        }
+    }
+}
